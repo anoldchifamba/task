e2b58e0af2ce34d6076e640ce8fdc5ee1a8cc817
unknown <anoldchifamba@gmail.com>
2024-08-09 16:27:30 +0200

    created task and usertask migration Commit

---
+++ database/migrations/2024_08_09_140903_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('title');
+            $table->string('description');
+            $table->date('duedate');
+            $table->integer('priority');
+            $table->integer('status');
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
