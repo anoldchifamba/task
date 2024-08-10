c0414954e2db0bc63ce815e9607010a8e284cfba
unknown <anoldchifamba@gmail.com>
2024-08-10 16:59:17 +0200

    removing other links like users and usertask  Commit

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,10 @@
 <!-- need to remove -->
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
-</li>
+</li> -->
 
 <li class="nav-item">
     <a href="{{ route('tasks.index') }}" class="nav-link {{ Request::is('tasks*') ? 'active' : '' }}">
@@ -15,18 +15,18 @@
 
 
 
-
+<!-- 
 <li class="nav-item">
     <a href="{{ route('users.index') }}" class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Users</p>
     </a>
-</li>
+</li> -->
 
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a href="{{ route('usertasks.index') }}" class="nav-link {{ Request::is('userTasks*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>User Tasks</p>
     </a>
-</li>
+</li> -->
 
